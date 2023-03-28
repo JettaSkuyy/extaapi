98155002954f4e04ed068dcce46be89d49bf5024
JettaSkuyy <78830663+JettaSkuyy@users.noreply.github.com>
2023-03-28 10:55:41 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 global.creator = 'Etzhy'// yourname
 global.MONGO_DB_URI = "mongodb+srv://etzyeww:11jetta06@cluster0.1kphz6e.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "ExtaApi" //isi apa aja bebas
-global.your_email = "jettaskuyy@gmail.com" //email
+global.your_email = "aylopyuhpolepel@gmail.com@gmail.com" //email
 global.email_password = "11jetta06" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
